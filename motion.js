0b5ff8e15dc9087638e73fc27bf7fd5dbe517f3b
SebastinSushi <101526592+SebastianSushi@users.noreply.github.com>
2022-11-15 18:43:11 +0500

    small changes

--- motion.js
+++ motion.js
@@ -6,6 +6,6 @@ let timerWH = setTimeout(function tick() {
     var windowOuterWidth = window.outerWidth
     var windowOuterHeight = window.outerHeight;
     var wh =document.getElementById("footWH")
-    wh.innerText = ("Width: " + windowOuterWidth + ", " + "Height: " + windowOuterHeight)
+    wh.innerText = ("Width: " + windowOuterWidth + "px, " + "Height: " + windowOuterHeight + "px")
     timerId = setTimeout(tick, 200);
   }, 20);
