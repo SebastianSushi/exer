b3e9299b28c2c2200f7ff044452373b023903434
SebastinSushi <101526592+SebastianSushi@users.noreply.github.com>
2022-11-15 18:40:41 +0500

    footer width height done

--- motion.js
+++ motion.js
@@ -2,3 +2,10 @@ function tearDrop () {
     var tearRed = document.getElementById("drop")
     tearRed.classList.toggle("tearred")
 }
+let timerWH = setTimeout(function tick() {
+    var windowOuterWidth = window.outerWidth
+    var windowOuterHeight = window.outerHeight;
+    var wh =document.getElementById("footWH")
+    wh.innerText = ("Width: " + windowOuterWidth + ", " + "Height: " + windowOuterHeight)
+    timerId = setTimeout(tick, 200);
+  }, 20);
