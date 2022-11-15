3cf563b768601a997596695c19ac44f31e1b8c88
SebastinSushi <101526592+SebastianSushi@users.noreply.github.com>
2022-11-15 19:44:41 +0500

    animation on button in work

--- motion.js
+++ motion.js
@@ -1,11 +1,14 @@
 function tearDrop () {
     var tearRed = document.getElementById("drop")
+    var invisButton = document.getElementById("buttontear")
+    invisButton.classList.toggle("buttonimginvis")
     tearRed.classList.toggle("tearred")
 }
+
 let timerWH = setTimeout(function tick() {
     var windowOuterWidth = window.outerWidth
     var windowOuterHeight = window.outerHeight;
-    var wh =document.getElementById("footWH")
+    var wh = document.getElementById("footWH")
     wh.innerText = ("Width: " + windowOuterWidth + "px, " + "Height: " + windowOuterHeight + "px")
-    timerId = setTimeout(tick, 200);
+    timerWH = setTimeout(tick, 200);
   }, 20);
