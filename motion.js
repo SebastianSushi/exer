437c92bd9fa87c0a619d2bce1ce110d1b574351e
SebastinSushi <101526592+SebastianSushi@users.noreply.github.com>
2022-11-16 18:28:02 +0500

    window on resize done

--- motion.js
+++ motion.js
@@ -1,14 +1,6 @@
 function tearDrop () {
-    var tearRed = document.getElementById("drop")
-    var invisButton = document.getElementById("buttontear")
-    invisButton.classList.toggle("buttonimginvis")
-    tearRed.classList.toggle("tearred")
+    var tearRed = document.getElementById("drop");
+    var invisButton = document.getElementById("buttontear");
+    invisButton.classList.toggle("buttonimginvis");
+    tearRed.classList.toggle("tearred");
 }
-
-let timerWH = setTimeout(function tick() {
-    var windowOuterWidth = window.outerWidth
-    var windowOuterHeight = window.outerHeight;
-    var wh = document.getElementById("footWH")
-    wh.innerText = ("Width: " + windowOuterWidth + "px, " + "Height: " + windowOuterHeight + "px")
-    timerWH = setTimeout(tick, 200);
-  }, 20);
